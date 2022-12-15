67e2fcd47756f0ce9524c9cbc8f6a197707b9123
Isaac Saxonov <89880964+clewner@users.noreply.github.com>
2022-12-15 10:35:53 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,7 +9,18 @@ var isMobile;
 
 $(document).ready(function() {
 
+  var header = $("#dropdownheader");
     
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > $("#navbar").height()) {
+        header.addClass("fixed");
+        header.slideDown();
+      } else {
+        header.slideUp(100, function() {
+          header.removeClass("fixed");
+        });
+      }
+    }); 
     
     if(isMobile){
       $("h1").css("letter-spacing", "-2px");
